907c3c29ca16722bb9894841157cb69664643d60
Smilucifer <fanyushe@msu.edu>
2017-02-28 21:25:39 -0500

    Zombies Simulation

---
+++ src/com/own/test/MyPanel.java
@@ -0,0 +1,5 @@
+package com.own.test;
+
+public class MyPanel {
+
+}
---
+++ src/com/own/test/city.java
@@ -0,0 +1,26 @@
+package com.own.test;
+
+public class city {
+	double width;
+	double length;
+	double um_x;
+	double um_y;
+	double safe_x;
+	double safe_y;
+	
+	//Consturctor
+	public city(double x,double y){
+		this.width=x;
+		this.length=y;
+	}
+	
+	public void set_um(double x, double y){
+		this.um_x=x;
+		this.um_y=y;
+	}
+	
+	public void set_safe(double x,double y){
+		this.safe_x=x;
+		this.safe_y=y;
+	}
+}
